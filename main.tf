73853ba6cb51dc2e8c92c092166251cf107f163e
André Leocádio <55164367+leocadioandre@users.noreply.github.com>
2024-11-10 18:59:40 -0300

    update secrets

--- main.tf
+++ main.tf
@@ -2,16 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# VPC configuration
 resource "aws_vpc" "techshop_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Subnet configuration
 resource "aws_subnet" "techshop_subnet" {
   vpc_id            = aws_vpc.techshop_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
 
+# Security group configuration
 resource "aws_security_group" "techshop_sg" {
   vpc_id = aws_vpc.techshop_vpc.id
 
@@ -30,13 +33,22 @@ resource "aws_security_group" "techshop_sg" {
   }
 }
 
+# EC2 instance configuration
 resource "aws_instance" "techshop_instance" {
-  ami             = "ami-0c55b159cbfafe1f0"
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.techshop_subnet.id
-  security_groups = [aws_security_group.techshop_sg.name]
+  ami                         = "ami-0c55b159cbfafe1f0"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.techshop_subnet.id
+  security_groups             = [aws_security_group.techshop_sg.name]
+  associate_public_ip_address = true
 
   tags = {
     Name = "TechShop-Instance"
   }
 }
+
+# Output the public IP of the EC2 instance
+output "ec2_public_ip" {
+  value       = aws_instance.techshop_instance.public_ip
+  description = "The public IP address of the TechShop EC2 instance."
+}
+
