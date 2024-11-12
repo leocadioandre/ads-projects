b3ffa5ff7eedc8e5e232d3db78b4db3178209d96
André Leocádio <55164367+leocadioandre@users.noreply.github.com>
2024-11-12 18:47:21 -0300

    update output.tf

--- main.tf
+++ main.tf
@@ -48,9 +48,5 @@ resource "aws_instance" "techshop_instance" {
   }
 }
 
-# Output the public IP of the EC2 instance
-output "ec2_public_ip" {
-  value       = aws_instance.techshop_instance.public_ip
-  description = "The public IP address of the TechShop EC2 instance."
-}
+
 
