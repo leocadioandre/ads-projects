c1bb332ecf26b6a3ca5c29d698c8bf2c70cddb42
André Leocádio <55164367+leocadioandre@users.noreply.github.com>
2024-11-12 20:18:28 -0300

    update

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_instance" "techshop_instance" {
   subnet_id                   = aws_subnet.techshop_subnet.id
   vpc_security_group_ids =    [aws_security_group.techshop_sg.id]
   associate_public_ip_address = true
-  key_name                    = var.key_name
+  #key_name                    = var.key_name
 
   tags = {
     Name = "TechShop-Instance"
