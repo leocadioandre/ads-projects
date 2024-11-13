7a0d4b35d7ca48915ccf6a032e8a4169fbb946cf
André Leocádio <55164367+leocadioandre@users.noreply.github.com>
2024-11-13 16:27:50 -0300

    refatorando teste1

--- infra/main.tf
+++ infra/main.tf
@@ -4,35 +4,19 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-# VPC configuration
-resource "aws_vpc" "techshop_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
-# Subnet configuration
-resource "aws_subnet" "techshop_subnet" {
-  vpc_id            = aws_vpc.techshop_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-}
+resource "aws_security_group" "allow_ssh" {
+  name_prefix = "allow_ssh"
 
-# Security group configuration
-resource "aws_security_group" "techshop_sg" {
-  vpc_id = aws_vpc.techshop_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow SSH access
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # For GitHub Actions, you might restrict it further
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -45,12 +29,10 @@ resource "aws_security_group" "techshop_sg" {
 
 # EC2 instance configuration
 resource "aws_instance" "techshop_instance" {
-  ami                         = "ami-0866a3c8686eaeeba"
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.techshop_subnet.id
-  vpc_security_group_ids =    [aws_security_group.techshop_sg.id]
-  associate_public_ip_address = true
-  key_name                    = var.key_name
+  ami                    = "ami-0c55b159cbfafe1f0" # AMI Ubuntu
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  security_groups        = [aws_security_group.allow_ssh.name]
 
   tags = {
     Name = "TechShop-Instance"
